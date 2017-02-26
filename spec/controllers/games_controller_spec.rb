61d9a101b24e980567d2db512d7636d1606d0a88
Philip Bradley <pdbradley@gmail.com>
2017-02-26 18:33:50 -0500

    updating some unit and controller tests for game

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
 
+  describe 'update action' do
+    it 'adds a player to empty black player slot and redirects to the game' do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      game = create(:game_white_player)
+
+      patch :update, id: game
+
+      expect(assigns(:game).black_user_id).to eq(user.id)
+      expect(response).to redirect_to games_path(game)
+    end
+  end
+
+  describe 'create action' do
+
+    it 'creates a new game with a white player as the current user, and redirect to games' do
+      user = FactoryGirl.create(:user)
+      sign_in user
+
+      post :create, game: { name: 'example game' }
+
+      expect(Game.first.white_user).to eq user
+      expect(response).to redirect_to(game_path(Game.first))
+    end
+  end
+
 end
