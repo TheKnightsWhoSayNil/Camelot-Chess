9f689ea29b44863611070943b0cad5ce59be626c
lkcz04185 <lkcz04185@gmail.com>
2017-02-26 12:11:59 +0000

    diagonal_move

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -3,8 +3,31 @@ class Piece < ApplicationRecord
   belongs_to :game
   belongs_to :user
 
-  def legal_move?
-    # example of future logic
-    # for each piece
+  def within_chessboard?(x, y)
+    return true if x > 0 && y > 0 && x < 7 && y < 7
+  end
+  # should store all the possible diagonal moves 
+  # excluding starting position hence "if x > 0"
+  def diagonal_moves
+      coordinates = []
+      8.times do |x|
+        if x > 0 
+        coordinates << (x, x)
+        coordinates << (x, -x)
+        coordinates << (-x, x)
+        coordinates << (-x, -x)
+        end
+      end
+      return coordinates   
+  end
+  
+  def diagonal_valid_moves(x_position, y_position)
+    valid_move_coordinates = []
+    diagonal_moves.each do |x, y|
+      if within_chessboard(x_position + x, y_position + y)
+        valid_move_coordinates << ((x_position + x), y_position + y)
+      end
+    end
+    return valid_move_coordinates   
   end
 end
