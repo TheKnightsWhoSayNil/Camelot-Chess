75757d8f9a7d6c04f0f351b3fb92217db58e469b
lkcz04185 <lkcz04185@gmail.com>
2017-02-27 10:39:25 +0000

    diagonal, vertical, horizontal moves

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,10 +1,15 @@
 # Piece will hold all similar logic for all pieces.
 class Piece < ApplicationRecord
-  belongs_to :game
-  belongs_to :user
+   belongs_to :game
+   belongs_to :user
+
+  def initialize (x_position, y_position)
+    @x_position = x_position
+    @y_position = y_position
+  end
 
   def within_chessboard?(x, y)
-    return true if x > 0 && y > 0 && x < 7 && y < 7
+    return true if x >= 0 && y >= 0 && x <= 7 && y <= 7
   end
   # should store all the possible diagonal moves 
   # excluding starting position hence "if x > 0"
@@ -12,22 +17,68 @@ class Piece < ApplicationRecord
       coordinates = []
       8.times do |x|
         if x > 0 
-        coordinates << (x, x)
-        coordinates << (x, -x)
-        coordinates << (-x, x)
-        coordinates << (-x, -x)
+        coordinates << [x, x]
+        coordinates << [x, -x]
+        coordinates << [-x, x]
+        coordinates << [-x, -x]
         end
       end
       return coordinates   
   end
-  
+
   def diagonal_valid_moves(x_position, y_position)
     valid_move_coordinates = []
     diagonal_moves.each do |x, y|
-      if within_chessboard(x_position + x, y_position + y)
-        valid_move_coordinates << ((x_position + x), y_position + y)
+      if within_chessboard?(x_position + x, y_position + y)
+        valid_move_coordinates << [(x_position + x), (y_position + y)]
       end
     end
     return valid_move_coordinates   
   end
+
+ def horizontal_moves
+    coordinates = []
+      8.times do |x|
+        if x > 0 
+        coordinates << [x, 0]
+        coordinates << [-x, 0]
+        end
+      end
+      return coordinates
+  end
+
+  def horizontal_valid_moves(x_position, y_position)
+    valid_move_coordinates = []
+    horizontal_moves.each do |x, y|
+      if within_chessboard?(x_position + x, y_position + y)
+        valid_move_coordinates << [(x_position + x), (y_position + y)]
+      end
+    end
+    return valid_move_coordinates
+  end
+
+
+  def vertical_moves
+    coordinates = []
+      8.times do |y|
+        if y > 0 
+        coordinates << [0, y]
+        coordinates << [0, -y]
+        end
+      end
+      return coordinates
+  end
+
+  def vertical_valid_moves(x_position, y_position)
+    valid_move_coordinates = []
+    vertical_moves.each do |x, y|
+      if within_chessboard?(x_position + x, y_position + y)
+        valid_move_coordinates << [(x_position + x), (y_position + y)]
+      end
+    end
+    return valid_move_coordinates
+  end
+
+
 end
+
