813a8643e4853cf4feb5ade98b00994fc35c6006
Conner Hiatt <connerh@me.com>
2017-02-28 03:02:52 +0000

    added new game form, and buttons on home page

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :game do
-    name 'example game'
+    id 1
+    white_user_id 1
+
   end
 
   factory :game_white_player, class: Game do
@@ -9,7 +11,19 @@ FactoryGirl.define do
   end
 
   factory :user do
-    id 1
-    username 'Example name'
+    sequence :email do |n|
+      "someone#{n}@example.com"
+    end
+
+    password '123123'
+  end
+
+  factory :user_black do
+    sequence :email do |n|
+      "someone#{n}@example.com"
+    end
+    id 2
+
+    password '123123'
   end
 end
