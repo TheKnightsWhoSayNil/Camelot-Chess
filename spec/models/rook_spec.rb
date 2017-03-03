9bdfb54464beb3315dc19e0632ea30484719f237
kat-gie <gielenkatie@gmail.com>
2017-03-03 13:03:26 -0600

    rook logic & tests

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -1,11 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Rook, type: :model do
-
   describe 'valid_move?' do
 
-    context 'White Rook' do 
-      let(:color) { true }
-      let(:start_y) { 1 }
-      let(:pawn) { Rook.create(color: color, x_position: 1, y_position: start_y) }
+    rook = Rook.create(color: true, x_position: 4, y_position: 4)
+
+    it 'returns true if moving 2 spaces vertically' do 
+      expect(rook.valid_move?(4, 6)).to eq(true)
+    end 
+
+    it 'returns true if moving horizontally' do
+      expect(rook.valid_move?(8,4)).to eq(true)
+    end 
+
+    it 'returns false if trying to move diagonally' do
+      expect(rook.valid_move?(7,7)).to eq(false)
+    end 
+
+  end 
+
+end 
+
       
