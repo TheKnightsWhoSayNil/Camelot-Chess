f158d463c9440d752946b5b3881b37ebaf579ad3
tfreebern2 <tfreebern2@gmail.com>
2017-03-03 16:39:10 +0000

    added fill_board tests

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -16,16 +16,22 @@ RSpec.describe Game, type: :model do
         expect(Game.available).to eq []
       end
     end
+  end
 
-    def create_games_with_two_players
-      player_1 = FactoryGirl.create(:user)
-      player_2 = FactoryGirl.create(:user)
-      Game.create(white_user: player_1, black_user: player_2)
+  describe 'fill_board method' do
+    it 'should fill_board when game is created' do
+      create_game_with_one_players.fill_board
     end
+  end
 
-    def create_game_with_one_players
-      player_1 = FactoryGirl.create(:user)
-      Game.create(white_user: player_1)
-    end
+  def create_games_with_two_players
+    player_1 = FactoryGirl.create(:user)
+    player_2 = FactoryGirl.create(:user)
+    Game.create(white_user: player_1, black_user: player_2)
+  end
+
+  def create_game_with_one_players
+    player_1 = FactoryGirl.create(:user)
+    Game.create(white_user: player_1)
   end
 end
