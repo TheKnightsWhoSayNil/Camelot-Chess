6b72c3a9e5ca43bf15f58efbf953ed5251731e5d
Conner Hiatt <connerh@me.com>
2017-03-04 09:45:19 +0000

    removed game_id from the schema, replaced with name:string

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170304093733) do
+ActiveRecord::Schema.define(version: 20170304094359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "games", force: :cascade do |t|
-    t.integer  "game_id"
     t.integer  "white_user_id"
     t.integer  "black_user_id"
     t.integer  "game_winner"
