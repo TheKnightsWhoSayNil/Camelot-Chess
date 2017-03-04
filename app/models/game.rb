c4046f6b52d9083a26f81f298dd8428816096d45
Brandon Elder <brandonelder3@gmail.com>
2017-03-04 19:49:07 +0000

    images --skip-ci

--- app/models/game.rb
+++ app/models/game.rb
@@ -19,30 +19,30 @@ class Game < ApplicationRecord
   def fill_board
     # fill white pieces
     (0..7).each do |i|
-      Pawn.create(user_id: self.white_user_id, game_id: self.id, x_position: i, y_position: 6, color: true)
+      Pawn.create(user_id: self.white_user_id, game_id: self.id, x_position: i, y_position: 6, image: 'white_pawn.png',  color: true)
     end
 
-    Rook.create(user_id: self.white_user_id, game_id: self.id, x_position: 0, y_position: 7, image: 'white-rook.png', color: true)
-    Rook.create(user_id: self.white_user_id, game_id: self.id, x_position: 7, y_position: 7, image: 'white-rook.png', color: true)
-    Knight.create(user_id: self.white_user_id, game_id: self.id, x_position: 1, y_position: 7, image: 'white-knight.png', color: true)
-    Knight.create(user_id: self.white_user_id, game_id: self.id, x_position: 6, y_position: 7, image: 'white-knight.png', color: true)
-    Bishop.create(user_id: self.white_user_id, game_id: self.id, x_position: 2, y_position: 7, image: 'white-bishop.png', color: true)
-    Bishop.create(user_id: self.white_user_id, game_id: self.id, x_position: 5, y_position: 7, image: 'white-bishop.png', color: true)
-    Queen.create(user_id: self.white_user_id, game_id: self.id, x_position: 3, y_position: 7, image: 'white-queen.png', color: true)
-    King.create(user_id: self.white_user_id, game_id: self.id, x_position: 4, y_position: 7, image: 'white-king.png', color: true)
+    Rook.create(user_id: self.white_user_id, game_id: self.id, x_position: 0, y_position: 7, image: 'white_rook.png', color: true)
+    Rook.create(user_id: self.white_user_id, game_id: self.id, x_position: 7, y_position: 7, image: 'white_rook.png', color: true)
+    Knight.create(user_id: self.white_user_id, game_id: self.id, x_position: 1, y_position: 7, image: 'white_knight.png', color: true)
+    Knight.create(user_id: self.white_user_id, game_id: self.id, x_position: 6, y_position: 7, image: 'white_knight.png', color: true)
+    Bishop.create(user_id: self.white_user_id, game_id: self.id, x_position: 2, y_position: 7, image: 'white_bishop.png', color: true)
+    Bishop.create(user_id: self.white_user_id, game_id: self.id, x_position: 5, y_position: 7, image: 'white_bishop.png', color: true)
+    Queen.create(user_id: self.white_user_id, game_id: self.id, x_position: 3, y_position: 7, image: 'white_queen.png', color: true)
+    King.create(user_id: self.white_user_id, game_id: self.id, x_position: 4, y_position: 7, image: 'white_king.png', color: true)
 
     # fill black pieces
     (0..7).each do |i|
-      Pawn.create(user_id: self.black_user_id, game_id: self.id, x_position: i, y_position: 1, color: false)
+      Pawn.create(user_id: self.black_user_id, game_id: self.id, x_position: i, y_position: 1, image: 'black_pawn.png', color: false)
     end
 
-    Rook.create(user_id: self.black_user_id, game_id: self.id, x_position: 0, y_position: 0, image: 'black-rook.png', color: false)
-    Rook.create(user_id: self.black_user_id, game_id: self.id, x_position: 7, y_position: 0, image: 'black-rook.png', color: false)
-    Knight.create(user_id: self.black_user_id, game_id: self.id, x_position: 1, y_position: 0, image: 'black-knight.png', color: false)
-    Knight.create(user_id: self.black_user_id, game_id: self.id, x_position: 6, y_position: 0, image: 'black-knight.png', color: false)
-    Bishop.create(user_id: self.black_user_id, game_id: self.id, x_position: 2, y_position: 0, image: 'black-bishop.png', color: false)
-    Bishop.create(user_id: self.black_user_id, game_id: self.id, x_position: 5, y_position: 0, image: 'black-bishop.png', color: false)
-    Queen.create(user_id: self.black_user_id, game_id: self.id, x_position: 3, y_position: 0, image: 'black-queen.png', color: false)
-    King.create(user_id: self.black_user_id, game_id: self.id, x_position: 4, y_position: 0, image: 'black-king.png', color: false)
+    Rook.create(user_id: self.black_user_id, game_id: self.id, x_position: 0, y_position: 0, image: 'black_rook.png', color: false)
+    Rook.create(user_id: self.black_user_id, game_id: self.id, x_position: 7, y_position: 0, image: 'black_rook.png', color: false)
+    Knight.create(user_id: self.black_user_id, game_id: self.id, x_position: 1, y_position: 0, image: 'black_knight.png', color: false)
+    Knight.create(user_id: self.black_user_id, game_id: self.id, x_position: 6, y_position: 0, image: 'black_knight.png', color: false)
+    Bishop.create(user_id: self.black_user_id, game_id: self.id, x_position: 2, y_position: 0, image: 'black_bishop.png', color: false)
+    Bishop.create(user_id: self.black_user_id, game_id: self.id, x_position: 5, y_position: 0, image: 'black_bishop.png', color: false)
+    Queen.create(user_id: self.black_user_id, game_id: self.id, x_position: 3, y_position: 0, image: 'black_queen.png', color: false)
+    King.create(user_id: self.black_user_id, game_id: self.id, x_position: 4, y_position: 0, image: 'black_king.png', color: false)
   end
 end
