1ced7b54fdec570ca165f697e75d83348a85cc60
kat-gie <gielenkatie@gmail.com>
2017-03-04 18:39:12 -0600

    bishop valid move and tests

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Rook, type: :model do
     rook = Rook.create(color: true, x_position: 4, y_position: 4)
 
     it 'returns true if moving 2 spaces vertically' do 
-      expect(rook.valid_move?(4, 6)).to eq(true)
+      expect(rook.valid_move?(4,6)).to eq(true)
     end 
 
     it 'returns true if moving horizontally' do
