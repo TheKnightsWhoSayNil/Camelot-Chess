52ddbc0d0a321278229c011285cbf7153ba3b6e6
Conner Hiatt <connerh@me.com>
2017-03-07 05:39:24 +0000

    --skip-ci deleted leftovers in piece model

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -34,10 +34,6 @@ class Piece < ApplicationRecord
     (x >= 0 && y >= 0 && x <= 7 && y <= 7)
   end
 
-<<<<<<< HEAD
-
-=======
->>>>>>> dd39d0d24c08af4ee526949ad2fdf6a3689d9d34
   #should store all the possible diagonal moves
   # excluding starting position hence "if x > 0"
   def diagonal_moves
