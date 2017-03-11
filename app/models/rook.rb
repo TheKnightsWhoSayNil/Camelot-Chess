744668aa3643209a95616b0ecde49638d2b9bffe
kat-gie <gielenkatie@gmail.com>
2017-03-11 17:22:27 -0600

    fixing my assigned issues

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -2,20 +2,8 @@
 class Rook < Piece
   #rook = moves horizontal or vertical through any number of unoccupied tiles 
   def valid_move?(x, y) #return false if not valid move 
-    horizontal_move?(x,y) || vertical_move?(x,y) 
+    super(x, y)
+    horizontal_move?(x,y) || vertical_move?(x,y)
   end
 
-  def horizontal_move?(x, y)
-    if y_position == y && x_position != x
-      return true
-    end
-    false
-  end
-
-  def vertical_move?(x, y)
-    if x_position == x && y_position != y
-      return true
-    end
-    false
-  end
 end 
