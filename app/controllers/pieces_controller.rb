48991d1b8dac283b3237327ed5bfd2fead0894b0
tfreebern2 <tfreebern2@gmail.com>
2017-03-12 16:06:55 +0000

    Ready to merge with master. Passing test

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,25 +1,18 @@
 class PiecesController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :update]
-
+  
   def show
-    #show the board again
     @game = Game.find(params[:id])
     @pieces = @game.pieces
   end
 
   def update
-    @piece = Piece.find_by_id(params[:id])
+    @piece = Piece.find(params[:id])
     @game = @piece.game
-
-    # x = params[:x_position].to_i
-    # y = params[:y_position].to_i
 
     if @piece.update_attributes(piece_params)
       redirect_to game_path(@game)
     end
-    
   end
-    
 
   private
 
