fc930d0d775c2622fcb5ff57bbf8bff6fe8ed34d
lkcz04185 <lkcz04185@gmail.com>
2017-03-22 21:43:55 +0000

    controler update action amended

--- Gemfile
+++ Gemfile
@@ -5,7 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-ruby '2.4.0'
+ruby '2.3.1'
+
 
 gem 'figaro'
 
--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -3,29 +3,27 @@ class PiecesController < ApplicationController
   def show
     #show the board again
     @game = Game.find(params[:game_id])
-
-    # @game = Game.find(params[:id])
-
     @pieces = @game.pieces
   end
 
   def update
-
-    @piece = Piece.find_by_id(params[:id])
-    
-    @piece.update_attributes(piece_params)
-    if @piece.valid?
+    @piece = Piece.find_by_id(params[:id])    
+    if @piece.move_to!(@piece.x_position, @piece.y_position) == false      
+      render text: "Invalid move, please try again. ..."
+      #to do handle the reponse by drag&drop to revert to the starting position   
+    else     
       redirect_to game_path(@piece.game)
-    else
-      return :show, status: :not_acceptable
+    end    
+  end  
 
-    # @piece = Piece.find(params[:id])
-    # @game = @piece.game
+    
+    # @piece.update_attributes(piece_params)
+    # if @piece.valid?
+     # redirect_to game_path(@piece.game)
+    # else
+    #  return :show, status: :not_acceptable
+     
 
-    # if @piece.update_attributes(piece_params)
-     # redirect_to game_path(@game)
-
-    end
   end
 
   private
