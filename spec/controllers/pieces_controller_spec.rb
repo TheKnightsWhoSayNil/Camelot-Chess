9c5e8f3aa2271ca869c441af9956c33f58c4b2d3
tfreebern2 <tfreebern2@gmail.com>
2017-03-25 15:37:44 +0000

    Added update position for King in castling moves. Updated King and Rook State in method. Added State Unmoved to Fill_board method. Added Updated Attribute of State in move_to! method

--- spec/controllers/pieces_controller_spec.rb
+++ spec/controllers/pieces_controller_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe PiecesController, type: :controller do
       game = FactoryGirl.create(:game, white_user_id: user1.id )
       piece = FactoryGirl.create(:piece, game: game, color: 'WHITE', x_position: 0, y_position: 0)
 
-      put :update, game_id: game.id, id: piece.id, piece: {x_position: 1, y_position: 1}
+      put :update, game_id: game.id, id: piece.id, piece: {x_position: 1, y_position: 1, state: 'moved'}
 
       expect(response).to have_http_status :success
     end
