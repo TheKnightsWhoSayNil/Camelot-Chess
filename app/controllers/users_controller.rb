ed4e4f4546add9ab6bbb56ca4f90e798ca04237b
Conner Hiatt <connerh@me.com>
2017-03-31 14:41:46 +0000

    fixed a few small things related to the user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 require 'pry'
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
   def show
    @my_games = my_games
    @open_games = open_games
@@ -8,13 +9,13 @@ class UsersController < ApplicationController
 
  def my_games
    if user_signed_in?
-     @my_games = Game.where('black_user_id = ? or white_user_id = ?', current_user.id, current_user.id)
+     @my_games = Game.where(white_user_id: current_user.id) || Game.where(black_user_id: current_user.id) 
    end
  end
 
  def open_games
    if user_signed_in?
-     @open_games = Game.where(black_user_id: nil).where.not(white_user_id: current_user.id)
+     @open_games = Game.where(black_user_id: nil) && Game.where(white_user_id: current_user.id)
    end
  end
 end
